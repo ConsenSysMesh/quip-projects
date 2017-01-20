7ab69bdd5315969ea81118320ecdc23066fdf7e5
dakpek <dorukakpek@gmail.com>
2017-01-20 13:31:16 -0500

    uploads document to my private folder, does not add people, does not add to Standup folder

--- index.py
+++ index.py
@@ -11,15 +11,21 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 client = quip.QuipClient(access_token="VUZlQU1BN0tZajk=|1516466133|whq6sTaYVldRrNWFs8Nw584tbXpgenX0rJgizoIHvUg=")
+content = '<h1>Today\'s automated Standup</h1><h2>This document has been added using the Quip API</h2><p>@Doruk Akpek</p><p>@Igor Lilic</p>'
+
 
 def run():
 	# client = quip.QuipClient(access_token="VUZlQU1BN0tZajk=|1516466133|whq6sTaYVldRrNWFs8Nw584tbXpgenX0rJgizoIHvUg=")
-	
 	newdocument()
+	
 
 
-def newdocument():
-	client.new_document("<h1>Hello World</h1><p>Let's see if this works</p>")
+def newdocument(format = "html", title = "Testing Stand-Up Automation", member_ids = ["VHPAOAvDhW2"]):
+	
+	client.new_document(content, member_ids)
+
+
 
 
 run()
+
